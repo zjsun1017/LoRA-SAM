de2b280f5d332f6c125693939f6a87b0e2ce8b81
RexRuijunZhang <zruijun@seas.upenn.edu>
2024-11-25 23:31:39 +0800

    First training

--- loralib/lora.py
+++ loralib/lora.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from layers import ConvLoRA
 import math
 
 
@@ -13,6 +12,7 @@ class LoRAedLinear(nn.Module):
         drop_out:float = 0,
         alpha:float=1.0,
     ):
+        super(LoRAedLinear, self).__init__()
         in_features = dense.in_features
         out_features = dense.out_features
         self.dense = dense
@@ -23,11 +23,9 @@ class LoRAedLinear(nn.Module):
 
         # Actual trainable parameters
         if rank > 0:
-            self.lora_A = nn.Parameter(self.weight.new_zeros((rank, in_features)))
-            self.lora_B = nn.Parameter(self.weight.new_zeros((out_features, rank)))
+            self.lora_A = nn.Parameter(self.dense.weight.new_zeros((rank, in_features),dtype=torch.float32))
+            self.lora_B = nn.Parameter(self.dense.weight.new_zeros((out_features, rank),dtype=torch.float32))
             self.scaling = alpha / rank
-            # Freezing the pre-trained weight matrix
-        self.dense.requires_grad_=False
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -39,8 +37,8 @@ class LoRAedLinear(nn.Module):
     
     def forward(self, x: torch.Tensor):
         result = self.dense(x)           
-        result += (self.lora_dropout(x) @ self.lora_A.transpose(0, 1) @ self.lora_B.transpose(0, 1)) * self.scaling
-        return result
+        return result + (self.lora_dropout(x) @ self.lora_A.transpose(0, 1) @ self.lora_B.transpose(0, 1)) * self.scaling
+
     
 class LoRAedconv(nn.Module):
     def __init__(self,
@@ -54,7 +52,48 @@ class LoRAedconv(nn.Module):
         out_channels = conv.out_channels
         self.conv = conv
 
-        assert isinstance(kernel_size, int)
+        if not isinstance(kernel_size, int):
+            kernel_size = kernel_size[0]
+        # Actual trainable parameters
+        if rank > 0:
+            self.lora_A = nn.Parameter(
+                self.conv.weight.new_zeros((rank * kernel_size, in_channels * kernel_size),dtype=torch.float32)
+            )
+            self.lora_B = nn.Parameter(
+              self.conv.weight.new_zeros((out_channels//self.conv.groups*kernel_size, rank * kernel_size),dtype=torch.float32)
+            )
+            self.scaling = alpha / rank
+            # Freezing the pre-trained weight matrix
+        self.reset_parameters()
+        self.merged = False
+
+    def reset_parameters(self):
+        if hasattr(self, 'lora_A'):
+            # initialize A the same way as the default for nn.Linear and B to zero
+            nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
+            nn.init.zeros_(self.lora_B)
+
+    def forward(self, x):
+        return self.conv._conv_forward(
+            x, 
+            self.conv.weight + (self.lora_B @ self.lora_A).view(self.conv.weight.shape) * self.scaling,
+            self.conv.bias
+        )
+    
+class LoRAedconvT(nn.Module):
+    def __init__(self,
+                conv: nn.ConvTranspose2d,
+                rank: int=4,
+                alpha: float=1.0):
+        super(LoRAedconvT, self).__init__()
+        
+        kernel_size = conv.kernel_size
+        in_channels = conv.in_channels
+        out_channels = conv.out_channels
+        self.conv = conv
+
+        if not isinstance(kernel_size, int):
+            kernel_size = kernel_size[0]
         # Actual trainable parameters
         if rank > 0:
             self.lora_A = nn.Parameter(
@@ -63,9 +102,8 @@ class LoRAedconv(nn.Module):
             self.lora_B = nn.Parameter(
               self.conv.weight.new_zeros((out_channels//self.conv.groups*kernel_size, rank * kernel_size))
             )
-            self.scaling = alpha / self.r
+            self.scaling = alpha / rank
             # Freezing the pre-trained weight matrix
-        self.conv.requires_grad = False
         self.reset_parameters()
         self.merged = False
 
@@ -83,17 +121,24 @@ class LoRAedconv(nn.Module):
         )
 
 
-def LoRA_injection(model:nn.Module, type):
+
+def LoRA_injection(model:nn.Module, type, rank, scale=1.0):
     for name, block in model.named_children():
-        if isinstance(block, nn.Linear) and type == 'linear':
-            block = LoRAedLinear(block, 4, 1)
+        if isinstance(block, nn.Linear) and 'linear' in type:
+            block = LoRAedLinear(block, rank, scale)
             setattr(model, name, block)
         
-        elif isinstance(block, nn.Conv2d) and type == 'conv':
+        elif isinstance(block, nn.Conv2d) and 'conv' in type:
+            min_channel = min(block.in_channels, block.out_channels)
+            if min_channel > rank:
+                block = LoRAedconv(block, rank, scale)
+                setattr(model, name, block)
+
+        elif isinstance(block, nn.ConvTranspose2d) and 'convT' in type:
             min_channel = min(block.in_channels, block.out_channels)
             if min_channel > 4:
-                block = ConvLoRA(block, 4, 1)
+                block = LoRAedconvT(block, rank, scale)
                 setattr(model, name, block)
 
         else:
-            LoRA_injection(block, type)
+            LoRA_injection(block, type, rank)
